78a722d9f1cc246538d89fa7ffe8b68649c68fd7
Jessica-Asamoah <jessicaasamoah903@gmail.com>
2024-02-12 01:38:01 +0000

    home

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -4,13 +4,79 @@ import './Header.css';
 import { headerBooks } from '../../Data/Data';
 
 // Import Swiper React components Import Swiper styles
+import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// React Arrow Icon
+import { GoArrowRight } from 'react-icons/go';
+import { GoArrowLeft } from 'react-icons/go';
+
+// Shape SVG Import
+import headerShape from '../../assets/header-shape.svg';
+
+//Import Link from React Router
+import { Link } from 'react-router-dom';
 
 export default function Header() {
 	return (
 		<header>
-			<div className="container header-container"></div>
+			<div className="header-container">
+				{/* Header Swiper Slider */}
+				<Swiper
+					spaceBetween={50}
+					slidesPerView={1}
+					loop={true}
+					modules={[Navigation, Pagination]}
+					pagination={{ el: '.swiper-pagination', clickable: true }}
+					navigation={{
+						preEl: '.button-prev-slide',
+						nextEl: '.button-next-slide',
+					}}
+				>
+					{headerBooks.map(({ title, info, img, btnLink }, index) => {
+						return (
+							<SwiperSlide key={index}>
+								<div className="header-wrapper container">
+									{/* Header Left */}
+									<div className="header-left">
+										<h1>{title}</h1>
+										<p dangerouslySetInnerHTML={{ __html: info }}></p>
+										<Link className="btn btn-border" to={'btnLink'}>
+											Learn More
+										</Link>
+									</div>
+									{/* Header Right */}
+									<div className="header-right">
+										<img src={img} alt="" />
+									</div>
+								</div>
+							</SwiperSlide>
+						);
+					})}
+
+					{/* Header Pagination */}
+					<div className="slider-button">
+						<div className="button-next-slide slidebutton">
+							<GoArrowRight />
+						</div>
+						<div className="button-prev-slide slidebutton">
+							<GoArrowLeft />
+						</div>
+					</div>
+					{/* Header Pagination */}
+					<div className="container">
+						<div className="swiper-pagination"></div>
+					</div>
+				</Swiper>
+
+				{/* J=Header Shape */}
+				<div className="header-shape">
+					<img src={headerShape} alt="" />
+				</div>
+			</div>
 		</header>
 	);
 }
