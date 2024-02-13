8a79626b17ff0aaa9abd36aa8f65f1b81cc97463
Jessica-Asamoah <jessicaasamoah903@gmail.com>
2024-02-13 00:56:53 +0000

    Brands

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 // Components Import
 import Header from '../Components/Header/Header';
+import Brands from '../Components/Brands/Brands';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
+			<Brands />
 		</>
 	);
 }
